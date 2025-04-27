77028edaac15787a50ed87cc45b34445ed368b70
RodrigoDiniz1 <rodrigodinizdigital@gmail.com>
2025-04-27 17:22:21 -0300

    att img

--- scripts.js
+++ scripts.js
@@ -44,15 +44,18 @@ function convertValues() {
 
 function changeCurrency(){
 const currencyName = document.getElementById('currency-name')
+const currencyImage = document.querySelector('.currency-img')
 
 if(currencySelect.value == 'dolar'){
 
 currencyName.innerHTML = 'Dólar americano'
+currencyImage.src = './assets/estados-unidos (1) 1.png'
 }
 
 if(currencySelect.value == 'euro'){
 
     currencyName.innerHTML = 'Euro'
+    currencyImage.src = './assets/euro.png'
     }
 
 }
